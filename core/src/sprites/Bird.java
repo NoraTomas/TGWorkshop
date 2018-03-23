290d33d281602eaf474a14f1238f6c3a5c1438ef
Nora Tomas <noratomas@hotmail.com>
2018-03-23 13:59:02 +0100

    changed bird textures to sprites

--- core/src/sprites/Bird.java
+++ core/src/sprites/Bird.java
@@ -2,25 +2,28 @@ package sprites;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector3;
 
 public class Bird {
     public static int GRAVITY = -15;
     private Vector3 position;
     private Vector3 velocity;
-    private Texture birdImage;
+    private Texture birdPNG;
+    private Sprite birdImage;
 
     public Bird(int x, int y){
         position = new Vector3(x, y, 0);
         velocity = new Vector3(0, 0, 0);
-        birdImage = new Texture("bird.png");
+        birdPNG = new Texture("bird.png");
+        birdImage = new Sprite(birdPNG);
     }
 
     public void update(float dt){
         velocity.add(0, GRAVITY, 0); //Adds Gavity to the y-component of velocity
         velocity.scl(dt); //dt = 0,017... So it scales down the power of gravity
 
-        
+
         position.add(0, velocity.y, 0); //Make position affected by velocity
 
         velocity.scl(1/dt); //Scale velocity back to normal
@@ -31,7 +34,7 @@ public class Bird {
         velocity.y = 420;
     }
 
-    public Texture getBirdImage(){
+    public Sprite getBirdImage(){
         return birdImage;
     }
 
